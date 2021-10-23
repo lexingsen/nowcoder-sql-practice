1158cfcc40e068e32541cb505b9489cdded172e6
lexingsen <lexingsen@163.com>
2021-10-23 09:05:23 +0800

    modify sql1

--- SQL1-查找最晚入职员工的所有信息.sql
+++ SQL1-查找最晚入职员工的所有信息.sql
@@ -1,4 +1,4 @@
 select * from employees order by hire_date desc limit 1;
 # 1.select * from employees 全表查询
-# 2. order by hire_date desc 根据入职时间降序查询
-# 获取第一个就是最晚入职的那一条信息
+# 2.order by hire_date desc 根据入职时间降序查询
+# 3.获取第一个就是最晚入职的那一条信息
